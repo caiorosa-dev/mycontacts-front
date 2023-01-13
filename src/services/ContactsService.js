e9691b7f5c3b87bd55433676ac03af9567f5ca5e
Bugado <addminplayerbr1122@gmail.com>
2023-01-13 19:12:12 -0300

    feat: Prodution Update

--- src/services/ContactsService.js
+++ src/services/ContactsService.js
@@ -1,11 +1,8 @@
 import HttpClient from './utils/HttpClient';
-import delay from '../utils/delay';
 
 class ContactsService {
   async list(orderBy = 'asc') {
     const { data } = await HttpClient.get(`/contacts?orderBy=${orderBy}`);
-
-    await delay(500);
 
     return data;
   }
@@ -13,15 +10,11 @@ class ContactsService {
   async get(id) {
     const { data } = await HttpClient.get(`/contacts/${id}`);
 
-    await delay(500);
-
     return data;
   }
 
   async create(contact) {
     const { data } = await HttpClient.post('/contacts', contact);
-
-    await delay(500);
 
     return data;
   }
@@ -29,15 +22,11 @@ class ContactsService {
   async update(contact) {
     const { data } = await HttpClient.put(`/contacts/${contact.id}`, contact);
 
-    await delay(500);
-
     return data;
   }
 
   async delete(id) {
     const { data } = await HttpClient.delete(`/contacts/${id}`);
-
-    await delay(500);
 
     return data;
   }
--- src/services/utils/HttpClient.js
+++ src/services/utils/HttpClient.js
@@ -1,5 +1,7 @@
 import axios from 'axios';
 
-axios.defaults.baseURL = 'http://localhost:3333';
+// 'http://localhost:3333'
+
+axios.defaults.baseURL = 'https://mycontacts-api.caiorosadev.com';
 
 export default axios;
